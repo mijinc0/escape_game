eba26184f696cded996d83060e4e547012abaf34
MIJ <mijinco1024@gmail.com>
2020-06-17 20:06:39 +0900

    add newUi pesudoCode2

--- src/ts/core/newUi/pseudoCode.ts
+++ src/ts/core/newUi/pseudoCode.ts
@@ -16,24 +16,26 @@ const mainWindow = root.add.box({x: 410, y: 10, width: 400, height: 400});
 
 const mainMenu = menuMainBox.add.container({x: 10, y: 10, width: 160, height: 380, align: 'down', margin: 10});
 
-const itemButton = mainMenuContainer.add.button({width: 160, height: 40, text: 'items'});
-
-itemButton.addSelectEvent((itemButton: IElement, selector: ISelector) => {
-  const itemList = new ItemList({items: GameGlobal.ownItems, x: 10, y: 0, width: 100, height: 300});
-  mainWindow.add.existing(itemList);
+const itemButton = mainMenuContainer.add.button({
+  width: 160,
+  height: 40,
+  text: 'items',
+  select: (itemButton: IElement, selector: ISelector) => {
+    const itemList = new ItemList({items: GameGlobal.ownItems, x: 10, y: 0, width: 100, height: 300});
+    mainWindow.add.existing(itemList);
+    
+    const itemDescribe = new ItemDescribe({x: 100, y: 100, widht: 100, height: 400});
+    mainWindow.add.existing(itemDescribe);
   
-  const itemDescribe = new ItemDescribe({x: 100, y: 100, widht: 100, height: 400});
-  mainWindow.add.existing(itemDescribe);
-
-  // 同じ名前のフィールドをバインドさせる(一方向バインド。双方向はクソコードを生みそうなので不要)
-  PropertyBinder.bind(itemList, itemDescribe, 'currentItem');
-
-  // セレクタの管理対象がItemListに変わる
-  // セレクタのキャンセルイベントが発生した時に戻る位置は、セレクタの中に保存する
-  // (配列でスタックを作って取り出す)
-  selector.setContainer({container: itemList, destroyIfCanceled: true});
+    // 同じ名前のフィールドをバインドさせる(一方向バインド。双方向はクソコードを生みそうなので不要)
+    PropertyBinder.bind(itemList, itemDescribe, 'currentItem');
+  
+    // セレクタの管理対象がItemListに変わる
+    // セレクタのキャンセルイベントが発生した時に戻る位置は、セレクタの中に保存する
+    // (配列でスタックを作って取り出す)
+    selector.setContainer({container: itemList, destroyIfCanceled: [itemList, itemDescribe]});
+  },
 });
-
 
 class ItemList extends ArrayMapContainer<Item> {
   currentItem: Item;
