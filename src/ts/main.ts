6db6cc2ea83b28efadc6f4ad7b67071478a6dadd
MIJ <mijinco1024@gmail.com>
2020-08-17 17:22:15 +0900

    update index.html

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,13 +4,14 @@ import { Boot } from './Boot';
 window.addEventListener('load', () => {
   // 多くのブラウザ環境下でオーディオを再生するためには、ユーザーの操作によりスクリプトを発火させる必要があるので
   // window(load)のではなくbutton(click)でゲームがスタートするようになっている(関連:ブラウザの自動再生ポリシー)
+  const launcher: any = document.body.querySelector('#launcher');
   const button: any = document.body.querySelector('#start_button');
 
   button.addEventListener(
     'click',
     () => {
       button.disable = true;
-      button.remove();
+      launcher.remove();
 
       const game: Boot = new Boot();
 
